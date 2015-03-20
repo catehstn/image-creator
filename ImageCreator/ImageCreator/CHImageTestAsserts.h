5a62bd0d7d9548751832ee8a5d722bce2b896d89
Cate Huston <catehuston@gmail.com>
2015-03-20 17:49:25 +0000

    adding comments

--- ImageCreator/ImageCreator/CHImageTestAsserts.h
+++ ImageCreator/ImageCreator/CHImageTestAsserts.h
@@ -3,7 +3,14 @@
 
 @interface CHImageTestAsserts : NSObject
 
+/**
+ * Asserts two color arrays equal: same number of colors, compares R, G, B, and A values for each.
+ */
 extern void ICAssertEqualColorArrayByHue(NSArray *array, NSArray *expected, CGFloat accuracy);
+
+/**
+ * Asserts two color arrays equal: same number of colors, compares H, S, B, and A values for each.
+ */
 extern void ICAssertEqualColorArrayByRGB(NSArray *array, NSArray *expected, CGFloat accuracy);
 
 @end
