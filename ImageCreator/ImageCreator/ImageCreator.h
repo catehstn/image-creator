f0f194113019f89d939b9d70145f46384191e6f9
Cate Huston <catehuston@gmail.com>
2015-03-20 17:47:27 +0000

    deleting once file

--- ImageCreator/ImageCreator/ImageCreator.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-//! Project version number for ImageCreator.
-FOUNDATION_EXPORT double ImageCreatorVersionNumber;
-
-//! Project version string for ImageCreator.
-FOUNDATION_EXPORT const unsigned char ImageCreatorVersionString[];
-
-#import <ImageCreator/CHImageCreator.h>
