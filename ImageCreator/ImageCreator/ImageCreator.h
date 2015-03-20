ca9ac37e0db31c49346fda564aea3cda4e1cb446
Cate Huston <catehuston@gmail.com>
2015-03-20 10:58:35 +0000

    adding empty project

---
+++ ImageCreator/ImageCreator/ImageCreator.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageCreator.
+FOUNDATION_EXPORT double ImageCreatorVersionNumber;
+
+//! Project version string for ImageCreator.
+FOUNDATION_EXPORT const unsigned char ImageCreatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageCreator/PublicHeader.h>
+
+
